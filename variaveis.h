1127790d538e912ae0f2fba1541dda491177aadd
JMelo <41171438+Josuka@users.noreply.github.com>
2018-11-05 08:01:30 -0200

    esses arquivos são novos

---
+++ variaveis.h
@@ -0,0 +1,13 @@
+const int LARGURA_TELA = 1280;
+const int ALTURA_TELA = 720;
+const int fontSize = 42;
+
+ALLEGRO_DISPLAY *janela = NULL;
+ALLEGRO_EVENT_QUEUE *fila_eventos = NULL;
+ALLEGRO_BITMAP *tela_inicio = NULL;
+ALLEGRO_BITMAP *transicaoTerra = NULL;
+ALLEGRO_BITMAP *boas_vindas[2] = {NULL, NULL};
+ALLEGRO_BITMAP *apresentacao[3] = {NULL, NULL, NULL};
+ALLEGRO_FONT *fonte = NULL;
+ALLEGRO_COLOR *cor_fonte_principal = NULL;
+
